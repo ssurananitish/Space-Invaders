feb11312cf783e5668465d35c06743884aa67cd5
Nitish Surana <surananitish@gmail.com>
2019-04-05 22:17:18 +0530

    Update repo

---
+++ Assets/Scripts/TargetMover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetMover : MonoBehaviour {
+
+	// define the possible states through an enumeration
+	public enum motionDirections {Horizontal, Vertical};
+	
+	// store the state
+	public motionDirections motionState = motionDirections.Horizontal;
+
+	// motion parameters
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+
+		// do the appropriate motion based on the motionState
+		switch(motionState) {
+			case motionDirections.Vertical:
+				// move up and down over time
+				gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+				break;
+
+            case motionDirections.Horizontal:
+                // move up and down over time
+                gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Space Invaders
+
+A classic first person shooter set in Outer Space.
+
+Game can be build Final_Build -> index.html (Open in Firefox, Edge)
+
+Chrome does not support WebGL format
